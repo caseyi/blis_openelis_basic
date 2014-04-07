2aa826533a85f31bfbb2595493365120c36bc2aa
rkitsao <rkitsao@yahoo.com>
2014-04-08 02:55:17 +0300

    PDF export
    Modified pdf export options

--- htdocs/export/export_pdf.php
+++ htdocs/export/export_pdf.php
@@ -2,46 +2,56 @@
 include("../includes/db_lib.php");
 putUILog('export_pdf', 'X', basename($_SERVER['REQUEST_URI'], ".php"), 'X', 'X', 'X');
 //getting new instance 
-$pdfFile = new_pdf(); 
+$pdfFile = PDF_new(); 
 
-PDF_open_file($pdfFile, " "); 
-
+$PDFFilePath = "blisreport".$date.".pdf";
+if (!PDF_open_file($pdfFile, $PDFFilePath)){
+	die('Error: '.PDF_get_errmsg($pdfFile));
+    /*print error;
+    exit;*/
+};
 //document info 
-pdf_set_info($pdfFile, "Auther", "Ahmed Elbshry"); 
-pdf_set_info($pdfFile, "Creator", "Ahmed Elbshry"); 
-pdf_set_info($pdfFile, "Title", "PDFlib"); 
-pdf_set_info($pdfFile, "Subject", "Using PDFlib"); 
+PDF_set_info($pdfFile, "Auther", "Ahmed Elbshry"); 
+PDF_set_info($pdfFile, "Creator", "Ahmed Elbshry"); 
+PDF_set_info($pdfFile, "Title", "PDFlib"); 
+PDF_set_info($pdfFile, "Subject", "Using PDFlib"); 
 
 //starting our page and define the width and highet of the document 
-pdf_begin_page($pdfFile, 595, 842); 
+PDF_begin_page($pdfFile, 595, 842); 
 
 //check if Arial font is found, or exit 
-if($font = PDF_findfont($pdfFile, "Arial", "winansi", 1)) { 
+$fontdir = "C:\\WINDOWS\\Fonts"; 
+PDF_set_parameter($pdfFile, "FontOutline", "DefArial=$fontdir\\arial.ttf");
+if($font = PDF_findfont($pdfFile, "DefArial", "winansi", 1)) { 
     PDF_setfont($pdfFile, $font, 12); 
 } else { 
     echo ("Font Not Found!"); 
     PDF_end_page($pdfFile); 
     PDF_close($pdfFile); 
-    PDF_delete($pdfFile); 
+    //PDF_delete($pdfFile); 
     exit(); 
 } 
 
 //start writing from the point 50,780 
 PDF_show_xy($pdfFile, "This Text In Arial Font", 50, 780); 
-PDF_end_page($pdfFile); 
+PDF_end_page($pdfFile);
 PDF_close($pdfFile); 
+var_dump($pdfFile); die;
 
 //store the pdf document in $pdf 
+if (!PDF_get_buffer($pdfFile)) die(PDF_get_errmsg);
 $pdf = PDF_get_buffer($pdfFile); 
 //get  the len to tell the browser about it 
-$pdflen = strlen($pdfFile); 
+$pdflen = strlen($pdf); 
+die('Stopped here '.$pdflen);
 
 //telling the browser about the pdf document 
 header("Content-type: application/pdf"); 
 header("Content-length: $pdflen"); 
-header("Content-Disposition: inline; filename=phpMade.pdf"); 
+//header("Content-Disposition: inline; filename=phpMade.pdf"); 
+header("Content-Disposition: inline; filename=".$PDFFilePath); 
 //output the document 
 print($pdf); 
 //delete the object 
-PDF_delete($pdfFile); 
+PDF_close($pdfFile);
 ?>
