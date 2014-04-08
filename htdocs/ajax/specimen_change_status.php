5e2b75940aa4f7187e6cdaccc056a538115c1874
rkitsao <rkitsao@yahoo.com>
2014-04-08 03:03:08 +0300

    Specimen Change bug fix

--- htdocs/ajax/specimen_change_status.php
+++ htdocs/ajax/specimen_change_status.php
@@ -3,7 +3,7 @@
 # Changes the status of the specimen
 # Called via Ajax from sample collection
 
-include("../includes/db_lib.php");
+require_once("../includes/db_lib.php");
 $sid = $_REQUEST['sid'];
 $time_collected = date("Y-m-d H:i:s");
 set_specimen_status($sid, 0,$time_collected);
